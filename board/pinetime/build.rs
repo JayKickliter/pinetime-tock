1ad1309b78d1c8c7d7ff82a055c4328b80d09703
Jay Kickliter <jay@kickliter.com>
2020-03-29 14:32:39 -0700

    kernel: carve out more rom for debugability

--- board/pinetime/build.rs
+++ board/pinetime/build.rs
@@ -1,5 +1,4 @@
 fn main() {
     println!("cargo:rerun-if-changed=../tock/boards/kernel_layout.ld");
-    println!("cargo:rerun-if-changed=../tock/boards/nordic/nrf52832_chip_layout.ld");
     println!("cargo:rerun-if-changed=layout.ld");
 }
